a483856e7cfcfe0ac8847ee625d3bf196f1f4e05
Mateusz Grzyb <mat.grz1@gmail.com>
2018-03-31 15:06:56 +0200

    doctrine and stof packages

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,6 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
