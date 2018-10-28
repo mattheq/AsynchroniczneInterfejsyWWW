2b7d9cf0c359aceb43163c481c1c17b6303fd9ab
Mateusz Grzyb <mat.grz1@gmail.com>
2018-10-28 10:22:13 +0100

    change language

--- assets/js/i18n.js
+++ assets/js/i18n.js
@@ -1,20 +1,22 @@
 import i18n from "i18next";
 import { reactI18nextModule } from 'react-i18next';
-import polish from '../public/locales/pl/translation.json';
-
-const resources = {
-    pl: {
-        translation: polish
-    }
-};
+import LanguageDetector from 'i18next-browser-languagedetector';
+import pl from '../public/locales/pl/translation.json';
+import en from '../public/locales/en/translation.json';
 
 i18n
+    .use(LanguageDetector)
     .use(reactI18nextModule) // passes i18n down to react-i18next
     .init({
-        resources,
-        lng: "en",
-
-        keySeparator: false,
+        fallbackLng: "en",
+        debug: true,
+        react: {
+            wait: true
+        },
+        resources: {
+            pl: { translation: pl },
+            en: { translation: en }
+        }
 });
 
 export default i18n;
