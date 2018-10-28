20d8ee47d614d179e843c5d1b8f48eaa9acd5bb1
Mateusz Grzyb <mat.grz1@gmail.com>
2018-10-28 08:16:17 +0100

    PageNotFound component

--- assets/js/components/App/App.js
+++ assets/js/components/App/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, HashRouter, Redirect} from 'react-router-dom';
+import {Route, HashRouter, Redirect, Switch} from 'react-router-dom';
 
 import 'semantic-ui-css/semantic.min.css';
 import './../../../css/custom.css';
@@ -18,6 +18,7 @@ import Registration from '../Registration/Registration';
 import PrivateRoute from '../PrivateRoute/PrivateRoute';
 import AuthHelper from '../../helpers/AuthHelper';
 import Navbar from "../Layout/Navbar";
+import PageNotFound from "../PageNotFound/PageNotFound";
 import ContextProvider from "../ContextProvider/ContextProvider";
 
 class App extends React.Component {
@@ -31,28 +32,31 @@ class App extends React.Component {
             <ContextProvider>
                 <HashRouter>
                     <div>
-                        <Route path="/" component={Navbar} />
-                        <Route exact path="/" component={HomePage}/>
-                        <Route path="/items/view/:id" component={ItemView} />
-                        <PrivateRoute path="/items/update/:id" component={ItemUpdateForm}/>
-                        <PrivateRoute path="/items/add" component={ItemForm} />
-                        <Route exact path="/items" component={ItemList}/>
-                        <PrivateRoute path="/logout" component={Logout}/>
-                        <Route path="/login" render={() => (
-                            AuthHelper.isLoggedIn() ? (
-                                <Redirect to="/" />
-                            ) : (
-                                <Login />
-                            )
-                        )}/>
-                        <Route path="/signup" render={() => (
-                            AuthHelper.isLoggedIn() ? (
-                                <Redirect to="/" />
-                            ) : (
-                                <Registration />
-                            )
-                        )}/>
-                        <Route path="/" component={Footer} />
+                        <Route component={Navbar} />
+                        <Switch>
+                            <Route exact path="/" component={HomePage}/>
+                            <Route path="/items/view/:id" component={ItemView} />
+                            <PrivateRoute path="/items/update/:id" component={ItemUpdateForm}/>
+                            <PrivateRoute path="/items/add" component={ItemForm} />
+                            <Route exact path="/items" component={ItemList}/>
+                            <PrivateRoute path="/logout" component={Logout}/>
+                            <Route path="/login" render={() => (
+                                AuthHelper.isLoggedIn() ? (
+                                    <Redirect to="/" />
+                                ) : (
+                                    <Login />
+                                )
+                            )}/>
+                            <Route path="/signup" render={() => (
+                                AuthHelper.isLoggedIn() ? (
+                                    <Redirect to="/" />
+                                ) : (
+                                    <Registration />
+                                )
+                            )}/>
+                            <Route component={PageNotFound}/>
+                        </Switch>
+                        <Route component={Footer} />
                     </div>
                 </HashRouter>
             </ContextProvider>
